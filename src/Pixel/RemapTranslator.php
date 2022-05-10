613a181e2cd688c327265bf384a5f3d92608b89d
Quinn Finney <quinn.finney@gmail.com>
2022-05-09 23:10:17 -0700

    remap translator

---
+++ src/Pixel/RemapTranslator.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Voxl\Util\Pixel;
+
+use App\Analytics\AnalyticsTranslator;
+use Voxl\Util\Auth\AuthProperty;
+
+class RemapTranslator
+{
+    public static function pre_source($params, &$pre_source)
+    {
+        // start with baseline valid params
+        // note: vxl_campaign and vxl_set are not here anymore
+        $valid_params = [
+            'vxl_channel',
+            'vxl_ad',
+            'utm_source',
+            'utm_medium',
+            'utm_campaign',
+            'utm_content',
+            'utm_term',
+            'domain'
+        ];
+        // search for valid params in the data
+        // count how many you find, set ones you dont find to null
+        $pre_source = [];
+        $valid_found = 0;
+        foreach ($valid_params as $p) {
+            if (array_key_exists($p, $params)) {
+                $pre_source[$p] = $params[$p];
+                $valid_found++;
+            } else {
+                $pre_source[$p] = null;
+            }
+        }
+        return $valid_found;
+    }
+
+    public static function apply_remaps(&$pre_source, $params)
+    {
+        $remaps = self::get_remaps_for_property();
+
+        // {mapping: {key: , destination: }, definition: [ {key:, value:} ]}
+        $applied = false;
+        foreach ($remaps as $remap) {
+            if (!array_key_exists($remap['mapping']['key'], $params)) {
+                continue;
+            }
+            //check conditions
+            $fail = false;
+            foreach ($remap['mapping']['conditions'] as $condition) {
+                if (!array_key_exists($condition['key'], $params)) {
+                    $fail = true;
+                    break;
+                }
+                if ($params[$condition['key']] != $condition['value']) {
+                    $fail = true;
+                    break;
+                }
+            }
+            if ($fail) {
+                continue;
+            }
+
+            // only remap to valid source attributes
+            if ($remap['mapping']['destination'] != null && !array_key_exists(
+                    $remap['mapping']['destination'],
+                    $pre_source
+                )) {
+                continue;
+            }
+            $applied = true;
+            // perform the remap
+            if ($remap['mapping']['destination'] != null) {
+                $pre_source[$remap['mapping']['destination']] = $params[$remap['mapping']['key']];
+            }
+            // apply the definition
+            foreach ($remap['definition'] as $rule) {
+                if (!array_key_exists($rule['key'], $pre_source)) {
+                    continue;
+                }
+                $pre_source[$rule['key']] = $rule['value'];
+            }
+        }
+        return $applied;
+    }
+
+    public static function get_remaps_for_property()
+    {
+        $property = AuthProperty::property();
+        $remaps = $property->remaps;
+        $all_remaps = [];
+
+        foreach ($remaps as $remap) {
+            $remap = $remap->toArray();
+            $compiled = [];
+            $def = $remap['overrides'];
+            $compiled['definition'] = $def;
+            unset($remap['overrides']);
+            $compiled['mapping'] = $remap;
+            array_push($all_remaps, $compiled);
+        }
+
+        return $all_remaps;
+    }
+
+}
